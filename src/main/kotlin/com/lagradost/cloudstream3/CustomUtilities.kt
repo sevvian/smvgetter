dff8eabbe61f91ea115c7cacfc93c584adcd025d
repo user <user@user.com>
2025-08-08 17:43:43 -0700

    [dyad] Increasing network timeout to fix extractor errors - wrote 1 file(s)

--- src/main/kotlin/com/lagradost/cloudstream3/CustomUtilities.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/CustomUtilities.kt
@@ -10,7 +10,8 @@ import java.util.Base64
 val app = Requests(
     defaultHeaders = mapOf(
         "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0"
-    )
+    ),
+    timeout = 30_000 // Increased timeout to 30 seconds
 )
 
 /**
